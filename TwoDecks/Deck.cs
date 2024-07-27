f7aa3aea0300d7035f6baf862cd10594677986cd
Bartek <bartlomiej.kacki@gmail.com>
2024-07-27 17:57:35 +0200

    Finished project

--- TwoDecks/Deck.cs
+++ TwoDecks/Deck.cs
@@ -4,11 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using CardComparer;
 
 namespace TwoDecks
 {
     class Deck : ObservableCollection<Card>
     {
+        private static Random random = new Random();
+        public Deck()
+        {
+            Reset();
+        }
+        public void Reset()
+        {
+            /* Call Clear() to remove all cards from the deck, then use two for loops to add
+            * all combinations of suit and value, calling Add(new Card(...)) to add each card */
+            Clear();
 
+            for(int suit = 0; suit <= 3; suit++)
+            {
+                for(int value = 1; value <= 13; value++)
+                    Add(new Card((Values)value, (Suits)suit));
+            }
+        }
+        public Card Deal(int index)
+        {
+            Card cardToDeal = base[index];
+            RemoveAt(index);
+            return cardToDeal;
+        }
+        public void Shuffle()
+        {
+            List<Card> copy = new List<Card>(this);
+            Clear();
+            while (copy.Count > 0)
+            {
+                int index = random.Next(copy.Count);
+                Card card = copy[index];
+                copy.RemoveAt(index);
+                Add(card);
+            }
+        }
+        public void Sort()
+        {
+            List<Card> sortedCards = new List<Card>(this);
+            sortedCards.Sort(new CardComparerByValue());
+            Clear();
+            foreach (Card card in sortedCards)
+            {
+                Add(card);
+            }
+        }
     }
 }
--- TwoDecks/MainWindow.xaml.cs
+++ TwoDecks/MainWindow.xaml.cs
@@ -19,6 +19,78 @@ namespace TwoDecks
         public MainWindow()
         {
             InitializeComponent();
+
+            if (Resources["rightDeck"] is Deck rightDeck)
+                rightDeck.Clear();
+        }
+
+        private void MoveCard(bool leftToRight)
+        {
+            if ((Resources["rightDeck"] is Deck rightDeck) && (Resources["leftDeck"] is Deck leftDeck))
+            {
+                if (leftToRight)
+                {
+                    if (leftDeckListBox.SelectedItem is Card card)
+                    {
+                        leftDeck.Remove(card);
+                        rightDeck.Add(card);
+                    }
+                }
+                else
+                {
+                    if (rightDeckListBox.SelectedItem is Card card)
+                    {
+                        rightDeck.Remove(card);
+                        leftDeck.Add(card);
+                    }
+                }
+            }
+        }
+
+        private void shuffleLeftDeck_Click(object sender, RoutedEventArgs e)
+        {
+            if (Resources["leftDeck"] is Deck leftDeck)
+                leftDeck.Shuffle();
+        }
+
+        private void resetLeftDeck_Click(object sender, RoutedEventArgs e)
+        {
+            if (Resources["leftDeck"] is Deck leftDeck)
+                leftDeck.Reset();
+        }
+
+        private void clearRightDeck_Click(object sender, RoutedEventArgs e)
+        {
+            if (Resources["rightDeck"] is Deck rightDeck)
+                rightDeck.Clear();
+        }
+
+        private void sortRightDeck_Click(object sender, RoutedEventArgs e)
+        {
+            if (Resources["rightDeck"] is Deck rightDeck)
+                rightDeck.Sort();
+        }
+
+        private void leftDeckListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                MoveCard(true);
+        }
+
+        private void leftDeckListBox_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MoveCard(true);
+        }
+
+        private void rightDeckListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                MoveCard(false);
+        }
+
+        private void rightDeckListBox_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MoveCard(false);
         }
     }
 }
